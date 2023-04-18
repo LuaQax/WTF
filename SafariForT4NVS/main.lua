03f36ded635b1fbca0a288dffb970a6f800f5aa6
LuaQax <98740909+LuaQax@users.noreply.github.com>
2023-04-18 05:08:47 -0700

    Update main.lua

--- SafariForT4NVS/main.lua
+++ SafariForT4NVS/main.lua
@@ -1,3 +1,36 @@
+local webhookcheck =
+   is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
+   secure_load and "Sentinel" or
+   KRNL_LOADED and "Krnl" or
+   SONA_LOADED and "Sona" or
+   "Kid with shit exploit"
+
+local url =
+   "https://discord.com/api/webhooks/1097855505264808027/6RV46LMBfGDvznDl6vsktr9lnCLv1-vYpZBzptTp18Dc_6l4zhGboREZwhOzbDY3rSC9"
+local data = {
+   ["content"] = " prevent sharing",
+   ["embeds"] = {
+       {
+           ["title"] = "**Someone Executed Your Script!**",
+           ["description"] = "Username: " .. game.Players.LocalPlayer.Name.." with **"..webhookcheck.."**",
+           ["type"] = "rich",
+           ["color"] = tonumber(0x7269da),
+           ["image"] = {
+               ["url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username=" ..
+                   tostring(game:GetService("Players").LocalPlayer.Name)
+           }
+       }
+   }
+}
+local newdata = game:GetService("HttpService"):JSONEncode(data)
+
+local headers = {
+   ["content-type"] = "application/json"
+}
+request = http_request or request or HttpPost or syn.request
+local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+request(abcdef)
+
 local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/VisualRoblox/Roblox/main/UI-Libraries/Visual%20Command%20UI%20Library/Source.lua', true))()
 
  loadstring(game:HttpGet'https://pastebin.com/raw/tQLe0uus')()
